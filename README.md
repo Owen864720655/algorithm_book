385b4d6554f9ae63b84d86a5d7cb9fd2db353179
Owen864720655 <864720655@qq.com>
2017-04-09 17:25:45 +0800

    revise the folder structure

---
+++ README.md
@@ -0,0 +1,4 @@
+## Implementation of the algorithms in book **Introduction to Algorithm (3rd)**
+
+> Author: Lei Fang
+> Email: 864720655@qq.com
---
+++ codes/algorithms_java/src/com/fanglei/chapter_2_1_insertion_sort/InsertionSort.java
@@ -0,0 +1,10 @@
+package com.fanglei.chapter_2_1_insertion_sort;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
