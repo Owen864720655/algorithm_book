29374e57eb0de0e48195aaee1cc64b89634cfc13
Owen864720655 <864720655@qq.com>
2017-04-09 22:20:12 +0800

    revise codes

--- codes/algorithms_java/src/com/fanglei/chapter_2_1_insertion_sort/InsertionSort.java
+++ codes/algorithms_java/src/com/fanglei/chapter_2_1_insertion_sort/InsertionSort.java
@@ -1,10 +1,15 @@
 package com.fanglei.chapter_2_1_insertion_sort;
 
+/**
+ * @author Owen
+ *
+ */
 public class InsertionSort {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Hello World!");
 	}
 
 }
